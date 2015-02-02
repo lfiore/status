de47e53866886aa1ceb0a692df72efc5b3bb1aa3
Luciano Fiore <l.fiore@live.com>
2015-02-02 19:32:12 +0000

    quick fix to get service status working on shared hosting

--- conf.php
+++ conf.php
@@ -1,5 +1,10 @@
 <?php
 
+// If you use shared hosting, or the services monitor doesn't work, change this to your IP or domain name
+// Try it with localhost first however
+
+$domain = 'localhost';
+
 // add ports you would like to monitor below in the following format
 // 'Service Name' => 0 (where 0 = port number)
 
@@ -18,6 +23,7 @@ $services_list = [
 // 'Server Name' => '0.0.0.0' (IP address)
 // 'Server Name' => 'http://domain.com' (domain name)
 // keep in mind, if you use Cloudflare or the domain doesn't point directly to the server, you will need to use the actual server IP, not the domain name
+// If you use shared hosting, ping may be disabled by your host
 
 $servers_list = [
 	'Google' => 'http://google.co.uk'
